a81a1aa06e24be2aafe9c7bce3d1b4db8da12576
Sergey Zavgorodniy <istarion.rus@gmail.com>
2018-03-25 12:29:39 +0300

    Http example for manual tests

--- bdse-kvnode/src/main/java/ru/csc/bdse/model/KeyValueRecord.java
+++ bdse-kvnode/src/main/java/ru/csc/bdse/model/KeyValueRecord.java
@@ -7,13 +7,34 @@ import com.sleepycat.persist.model.PrimaryKey;
 public class KeyValueRecord {
     @PrimaryKey
     private String key;
+    private boolean isDeleted;
+    private long timestamp;
     private byte[] data;
 
-    private KeyValueRecord() { }
+    private KeyValueRecord() {
+        this.timestamp = System.currentTimeMillis();
+    }
 
     public KeyValueRecord(String key, byte[] data) {
         this.key = key;
         this.data = data;
+        this.timestamp = System.currentTimeMillis();
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
     }
 
     public String getKey() {
